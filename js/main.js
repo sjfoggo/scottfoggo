3178b37db48dea51f71f6df6308c05ec15f3608c
Scott F <s.foggo.7@gmail.com>
2014-11-14 23:13:55 -0500

    off canvas menu wahooo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+$( document ).ready(function() {
+   $('#navbar').scotchPanel({
+                    containerSelector: 'body',
+                    direction: 'left',
+                    duration: 400,
+                    transition: 'ease',
+                    clickSelector: '#toggle-nav',
+                    distanceX: '200px',
+                    enableEscapeKey: true
+                });
+	$("a").click(function(){
+	  $(this).toggleClass("open");
+	}); 
+});
