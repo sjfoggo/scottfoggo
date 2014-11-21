e7ba5e506ae5ab5d71a67f97cf37247142376b1c
Scott F <s.foggo.7@gmail.com>
2014-11-20 23:14:45 -0500

    removing nav plugin

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 $( document ).ready(function() {
-   $('#navbar').scotchPanel({
-                    containerSelector: 'body',
-                    direction: 'left',
-                    duration: 400,
-                    transition: 'ease',
-                    clickSelector: '#toggle-nav',
-                    distanceX: '200px',
-                    enableEscapeKey: true
-                });
-	$('#toggle-nav').click(function(){
-	  $(this).toggleClass("open");
-	});
-    $(".intro").typed({
+    $("#quickbio").typed({
         strings: ["bold.^1000", "passionate.^1000", "creative.^1000", "a developer/designer."],
         typeSpeed: 10,
         backSpeed: 20
