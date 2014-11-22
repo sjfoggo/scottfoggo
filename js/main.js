474a9f58374f1c047fc9d46d5ec03d96d92869a6
Scott F <s.foggo.7@gmail.com>
2014-11-21 23:34:55 -0500

    header complete and about work

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 $( document ).ready(function() {
+    $("#toggle-nav").click(function(){
+      $(this).toggleClass("open");
+    });
     $("#quickbio").typed({
         strings: ["bold.^1000", "passionate.^1000", "creative.^1000", "a developer/designer."],
         typeSpeed: 10,
         backSpeed: 20
-    }); 
+    });
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
