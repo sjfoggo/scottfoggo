852eab1012dfac47a2dd5de0659d0938b6c9344d
Scott F <s.foggo.7@gmail.com>
2014-11-22 16:58:40 -0500

    bw for now, added skill bars

--- js/main.js
+++ js/main.js
@@ -21,4 +21,9 @@ $( document ).ready(function() {
             }
         }
     });
+    $('.skillbar').each(function(){
+        $(this).find('.skillbar-bar').animate({
+        width:$(this).attr('data-percent')
+    }, 2000);
+});
 });
