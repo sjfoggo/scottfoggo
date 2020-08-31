fd77e1d76bcb0fc03273ec77eb468dfe64c0bc69
foggopogo <s.foggo.7@gmail.com>
2020-08-30 23:15:15 -0700

    Contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,17 @@
 import React from "react";
 import styles from "../css/Contact.module.css";
+import Row from "./common/Row";
 
 function Contact() {
   return (
-      <div className={styles.content}>
-        <p>You can reach me via email</p>
-        <p>LinkedIn</p>
-        <p>or checkout my Github</p>
-      </div>
+    <div className={styles.container}>
+      <Row>
+        <i class="fas fa-paper-plane"></i>
+        <i class="fab fa-linkedin"></i>
+        <i class="fab fa-github"></i>
+      </Row>
+    </div>
   );
-};
+}
 
 export default Contact;
