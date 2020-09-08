f62834e8b11a8c1f0689feffe0f9ceef1e3980f5
foggopogo <s.foggo.7@gmail.com>
2020-09-07 22:56:41 -0700

    Add career info

--- src/components/Career.js
+++ src/components/Career.js
@@ -2,8 +2,9 @@ import React from "react";
 import Column from "./common/Column";
 import Row from "./common/Row";
 import Text from "./common/Text";
+import Link from "./common/Link";
 
-import styles from "./../css/Career.module.css";
+import styles from "../css/Career.module.css";
 
 const ANALYTICS_LINK =
   "https://www.gartner.com/en/information-technology/glossary/augmented-analytics";
@@ -12,25 +13,30 @@ function Career() {
   return (
     <Row>
       <Column>
-        <div className={styles.imgContainer}>
-        <Text>Looking for:</Text>
-        <Text>Software eng roles working on data visualization and analysis.</Text>
-        <br />
-        <br />
-        <Text>Experienced in:</Text>
-        <Text>Frontend Web Stack</Text>
-        <Text>(JS / React / Redux / GraphQL / Relay / PHP / Hack)</Text>
-        <Text>Backend/Data Science Stack (Python)</Text>
-        <Text>Time series analysis and forecasting</Text>
+        <div className={styles.textBlock}>
+          <Text>
+            I'm a software engineer currently working at Facebook. I have an
+            interest in data visualization and analysis.
+          </Text>
+          <br />
+          <Text>
+            Most recently I've been working on{" "}
+            <Link href={ANALYTICS_LINK}>augmented analytics</Link> tooling in
+            the forecasting, monitoring, and root cause analysis spaces.
+          </Text>
         </div>
       </Column>
       <Column>
-        <div className={styles.textContainer}>
-          <Text>
-            Work:
-          </Text>
+        <div className={styles.textBlock}>
+          <Text>Places I've worked</Text>
           <Text>Facebook</Text>
           <Text>Oculus</Text>
+          <br />
+          <br />
+          <Text>Frontend Web Stack</Text>
+          <Text>(JS / React / Redux / GraphQL / Relay / PHP / Hack)</Text>
+          <Text>Backend/Data Science Stack (Python)</Text>
+          <Text>Time series analysis and forecasting</Text>
         </div>
       </Column>
     </Row>
