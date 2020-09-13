16d0a070757e812ffd6981a47bb779dfb798eee0
foggopogo <s.foggo.7@gmail.com>
2020-09-13 16:07:14 -0700

    Updates

---
+++ precache-manifest.2ade585232e40b418ee9f4a714c81200.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f600a8c70b623c710ca6adbed10b27b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8a10ce1f58f3ca0eee1",
+    "url": "/static/css/main.62368be2.chunk.css"
+  },
+  {
+    "revision": "a28a09a5b35f6301f8b8",
+    "url": "/static/js/2.a40b41bf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.a40b41bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a10ce1f58f3ca0eee1",
+    "url": "/static/js/main.6487465e.chunk.js"
+  },
+  {
+    "revision": "69e398fca61b7614dfd3",
+    "url": "/static/js/runtime-main.71b078e6.js"
+  },
+  {
+    "revision": "dfefdced25e618596ec3f17f8e6f2f21",
+    "url": "/static/media/BigSur.dfefdced.png"
+  },
+  {
+    "revision": "a065ec20e749d0ab9781427e9d4b33b2",
+    "url": "/static/media/Profile.a065ec20.png"
+  },
+  {
+    "revision": "1486d34c818841c22a974017167edbfc",
+    "url": "/static/media/Redwood.1486d34c.png"
+  },
+  {
+    "revision": "a868bac6f0b9b5f335fd33f8931d0d6e",
+    "url": "/static/media/ScottFoggo-Resume.a868bac6.pdf"
+  },
+  {
+    "revision": "161c84bf3e5d053c75a702230f3829d7",
+    "url": "/static/media/Yosemite.161c84bf.png"
+  }
+]);
