15b1d971d596e4783a3df8576a09fd5bfbc49cc0
foggopogo <s.foggo.7@gmail.com>
2026-08-22 11:47:55 -0700

    Rework homepage introduction

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,37 +1,23 @@
 import React from "react";
-import Row from "./common/Row";
-import Column from "./common/Column";
-import Image from "./common/Image";
 import styles from "../css/About.module.css";
-
-import profile from "../assets/Profile.png";
 
 function About() {
   return (
-    <Row>
-      <Column>
-        <div className={styles.image}>
-          <Image alt="Profile" src={profile} />
-        </div>
-      </Column>
-      <Column>
-        <div className={styles.textBlock}>
-          <p>
-            I'm a software engineer, currently working
-            at Facebook. I have an interest in data science/engineering and a
-            goal of making state-of-the-art data analysis techniques accessible
-            for <b>everyone</b> to use.
-          </p>
-          <br />
-          <br />
-          <p>
-            Most recently, I've been working in the "agentic analytics" space,
-            building tooling for time series forecasting, monitoring, root cause
-            analysis, and reporting.
-          </p>
-        </div>
-      </Column>
-    </Row>
+    <div className={styles.about}>
+      <p className="eyebrow">About</p>
+      <div className={styles.content}>
+        <p className={styles.statement}>
+          I build software that makes <em>complex systems</em> feel clear,
+          useful, and human.
+        </p>
+        <p className={styles.detail}>
+          I’m a software engineer focused on the space where product thinking,
+          data, and dependable infrastructure meet. Most recently, that has meant
+          building tools for forecasting, monitoring, root-cause analysis, and
+          reporting.
+        </p>
+      </div>
+    </div>
   );
 }
 
